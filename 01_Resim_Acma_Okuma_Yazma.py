f9985ca7681d3168c1d28185d6e3c8f03ef1feff
Muhammed Selim <95688540+SelimDayanc@users.noreply.github.com>
2023-05-26 11:05:29 +0300

    cv2
    Görüntü Işleme

---
+++ 01_Resim_Acma_Okuma_Yazma.py
@@ -0,0 +1,20 @@
+""" -*- coding: utf-8 -*-
+    https://github.com/SelimDayanc?tab=repositories
+    https://www.linkedin.com/in/selim-dayan%C3%A7-b81a85235/ """
+
+import cv2
+
+#img2 = cv2.imread("750x750_siyah.jpg")
+img = cv2.imread("750x750.jpg",0)
+""" resmi img degiskenine atadik. '0' parametresi direkt olarak siyah-beyaza gecis yapar
+    print(type(img)) #->numpy.ndarray """
+
+cv2.imwrite("......./Desktop/750x750_siyah.jpg", img)
+#img degiskeni siyah-beyaz olarak masaustune kaydedildi
+
+cv2.imshow("Deneme Resmi", img) #resim ekranda gosterildi
+
+#cv2.imshow("Deneme Resmi", img) #resim ekranda gosterildi
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
